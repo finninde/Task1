5f7a3fa44c784c9e521d18b93e7205249d306918
Finn Inderhaug Holme <finninde@stud.ntnu.no>
2014-01-21 02:36:06 +0100

    Added dumy methods for MainThread

--- src/com/example/task1/MainThread.java
+++ src/com/example/task1/MainThread.java
@@ -4,5 +4,17 @@ public class MainThread extends Thread{
 
 	private boolean isRunning;
 	
+	public void setRunning(boolean running){
+		this.isRunning = isRunning; 
+	}
+	
+	@Override
+	public void run(){
+		while(isRunning){
+			//TODO: update game state, and render state to screen
+		}
+	}
+	
+	
 	
 }
