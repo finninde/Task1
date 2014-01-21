5629d2a3967d04995a985788ef44fb285395dee2
Finn Inderhaug Holme <finninde@stud.ntnu.no>
2014-01-21 03:02:12 +0100

    added debug feature for mainthread via logcat (Log.d())

--- src/com/example/task1/MainThread.java
+++ src/com/example/task1/MainThread.java
@@ -1,11 +1,15 @@
 package com.example.task1;
 
+import android.util.Log;
 import android.view.SurfaceHolder;
 
 public class MainThread extends Thread{
+	private static final String TAG = MainThread.class.getSimpleName(); // for logging purposes
+	
 	private SurfaceHolder surfaceHolder; 	// bare surfaceholder kan låse panelet mens vi tegner
 	private MainGamePanelView gamePanelView;
 	private boolean isRunning;
+
 	
 	public MainThread(SurfaceHolder surfaceHolder, MainGamePanelView gamePanelView){
 		super();
@@ -20,9 +24,13 @@ public class MainThread extends Thread{
 	
 	@Override
 	public void run(){
+		long tickCount = 0L;
+		Log.d(TAG,"Start main game loop :3");
 		while(isRunning){
-			//TODO: update game state, and render state to screen
+			tickCount++;
+			//TODO: update gamestate and render
 		}
+		Log.d(TAG,"GameLoop executed:  "+ tickCount + "times");
 	}
 	
 	
